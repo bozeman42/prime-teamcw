6785f0654ad3d2768874fa5466f953c36a10a071
David Beaudway <david.beaudway@gmail.com>
2017-12-07 16:39:53 -0600

    Moved office functionality to a service

---
+++ server/public/scripts/services/office.service.js
@@ -0,0 +1,27 @@
+myApp.service('OfficeService', function ($http, $location) {
+    console.log('OfficeService Loaded');
+    var self = this;
+    self.data = {
+        offices: ''
+    };
+    self.userObject = {};
+
+    self.createOffice = function(value) {
+        $http.post('/office', value).then(function(response){
+            console.log('Succesfully created office', response);
+        }).catch(function (err){
+            console.log('Error creating office', err)
+        })
+    };
+
+    self.getOffices = function() {
+        $http.get('/office').then(function(response){
+            self.data.offices = response.data;
+            console.log('Succesfully retrieved offices', self.data);
+            
+        }).catch(function (err){
+            console.log('Error retrieving offices', err)
+        })
+    }
+
+});
